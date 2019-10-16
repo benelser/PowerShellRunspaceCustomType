ac39ed5e2d3e0f833924bd56cd6ec94b2cb5904c
Ben Elser <benjamin.elser18@gmail.com>
2019-10-16 12:31:44 -0500

    init

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Threading;
+using CustomTypes;
+using System.Management.Automation;
+using System.Management.Automation.Runspaces;
+
+namespace PowerShellRunspaceCustomType
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create Person in main thread
+            Console.WriteLine("\nCreating Person in therad {0}", Thread.CurrentThread.ManagedThreadId);
+            Person me = new Person("Benjamin", "Elser", 32);
+            Console.WriteLine(me);
+            Console.WriteLine("------------------------------\n");
+
+            // Setup Runspace to load custom types
+            InitialSessionState iss = InitialSessionState.CreateDefault();
+            TypeData ct = new TypeData("CustomTypes.Person");
+            SessionStateTypeEntry sste = new SessionStateTypeEntry(ct, false);
+            iss.Types.Add(sste);
+            
+            using (PowerShell ps = PowerShell.Create(iss))
+            {
+                // create type in runspace from custom type Person invoking ToString() method
+                var results = ps.AddScript("using namespace CustomTypes;$t = [Person]::new('Benjamin', 'Elser', 32); $t.ToString(); [System.Threading.Thread]::CurrentThread.ManagedThreadId").Invoke();
+                Console.WriteLine("Creating Person in Runspace and getting TheadID");
+                foreach (var item in results)
+                {
+                    Console.WriteLine(item);
+                }
+                Console.WriteLine("------------------------------\n");
+                ps.Commands.Clear();
+                
+                // Explicitly Cast return object to type Class
+                Console.WriteLine("Creating Person in runspace...");
+                var psobjects = ps.AddScript("using namespace CustomTypes; $t = [Person]::new('Adam', 'Sandler', 32); $t; [System.Threading.Thread]::CurrentThread.ManagedThreadId").Invoke();
+                Person adamSandler = (Person)psobjects[0].ImmediateBaseObject;
+                int rsthread = (int)psobjects[1].ImmediateBaseObject;
+                System.Console.WriteLine("Person created in Runspace {0} --------- Runspace Thread {1}", adamSandler, rsthread);
+                Console.WriteLine("------------------------------\n");
+                Console.WriteLine("Explicitly casted Person object from Runspace first name: {0} in thread {1}\n", adamSandler.FirstName, Thread.CurrentThread.ManagedThreadId);
+            }
+
+        }
+            
+    }
+}
---
+++ README.md
@@ -0,0 +1,18 @@
+# Creating and Passing Custom Types to and from PowerShell Runspaces
+
+### Brief Synopsis:
+This code demonstrates setting up and creating runspaces with injected custom types using InitialSessionState property on the PowerShell Runspsace.
+
+### Initialize Runsapce with Custom Type
+This code assumes you have the Type Person created in Namespace CustomTypes and the class has the public modifier 
+```C#
+        InitialSessionState iss = InitialSessionState.CreateDefault();
+        TypeData ct = new TypeData("CustomTypes.Person");
+        SessionStateTypeEntry sste = new SessionStateTypeEntry(ct, false);
+        iss.Types.Add(sste);
+        PowerShell ps = PowerShell.Create(iss)
+```
+Looking at [Program.cs](./Program.cs) we see successfull creation of [Person objects](./People) in the runspaces and passing those Custom types around the application.
+
+#### Expected output
+[Output:](./output.jpg)
